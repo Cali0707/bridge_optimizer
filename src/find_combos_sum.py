84320907e10568d419410ab8906381e88854c090
Cali0707 <65566908+Cali0707@users.noreply.github.com>
2020-12-05 22:21:38 -0500

    Added comments

--- src/find_combos_sum.py
+++ src/find_combos_sum.py
@@ -12,12 +12,21 @@ def find_combos_sum(sum_to, max_val, length):
 def sum_less_than_area(min_width, max_width, max_area):
     # Assume length of 950mm
     length = 950
+
+    # make list containing every possible width
+    # starts at min_width, and cant go above half of the total width - 2 because height has to be at least 1
     widths = []
-    heights = []
     for i in range(min_width, (max_width // 2) - 2):
         widths.append(i)
+
+    # make list containing every possible height
+    # starts at 1, and goes to the half of the max width minus 2 * min width
+    heights = []
     for i in range(1, (max_width - (2 * min_width)) // 2):
         heights.append(i)
+
+    # return list of every (width, height) st. the sum of the areas formed by multiplying them by length is less than
+    # the area of the matboard
     return [p for p in product(widths, heights) if sum([p[0] * 2 * length, p[1] * 2 * length]) < max_area]
 
 
--- src/second_moment_of_area/q_i_beam.py
+++ src/second_moment_of_area/q_i_beam.py
@@ -3,7 +3,7 @@ def q_i_beam(half_mid_height, mid_width, top_height, top_width):
     top_centroid = half_mid_height + (top_height / 2)
     area_mid = half_mid_height * mid_width
     area_top = top_height * top_width
-    return (area_mid * mid_centroid) + (area_top * top_centroid)
+    return (area_mid * mid_centroid) + (area_top * top_centroid), (area_top * top_centroid)
 
 
 if __name__ == '__main__':
